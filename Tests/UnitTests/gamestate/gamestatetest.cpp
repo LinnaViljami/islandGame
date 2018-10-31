c55cc9e02f3cd685cca47845b44158a7178b0109
LinnaViljami <viljami.linna@gmail.com>
2018-10-31 12:58:03 +0200

    GameStateTest: Implemented testCurrentGamePhase

--- Tests/UnitTests/gamestate/gamestatetest.cpp
+++ Tests/UnitTests/gamestate/gamestatetest.cpp
@@ -5,6 +5,10 @@
 using Student::GameState;
 using Common::GamePhase;
 
+Q_DECLARE_METATYPE(GameState);
+Q_DECLARE_METATYPE(GamePhase);
+
+
 class GamestateTest : public QObject
 {
     Q_OBJECT
@@ -15,6 +19,8 @@ public:
 private Q_SLOTS:
     void testChangeGamePhase();
     void testChangePlayerTurn();
+    void testCurrentGamePhase();
+    void testCurrentGamePhase_data();
 };
 
 GamestateTest::GamestateTest()
@@ -35,6 +41,27 @@ void GamestateTest::testChangePlayerTurn()
     int testPlayerId = 2;
     testState.changePlayerTurn(2);
     QCOMPARE(testPlayerId, testState.currentPlayer());
+}
+
+void GamestateTest::testCurrentGamePhase()
+{
+    GamePhase defaultPhase = GamePhase::MOVEMENT;
+    QFETCH(GamePhase, expectedPhase);
+    QFETCH(GameState, newState);
+
+    QCOMPARE(newState.currentGamePhase(), defaultPhase);
+    newState.changeGamePhase(expectedPhase);
+    QCOMPARE(newState.currentGamePhase(), expectedPhase);
+}
+
+void GamestateTest::testCurrentGamePhase_data()
+{
+    QTest::addColumn<GamePhase>("expectedPhase");
+    QTest::addColumn<GameState>("newState");
+
+    QTest::newRow("Movement") << GamePhase::MOVEMENT << GameState();
+    QTest::newRow("Sinking") << GamePhase::SINKING << GameState();
+    QTest::newRow("Spinning") << GamePhase::SPINNING << GameState();
 }
 
 QTEST_APPLESS_MAIN(GamestateTest)
