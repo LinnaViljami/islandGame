3e892659b51ffddabcd36ac71ea2572e8c171969
Paulus Limma <paulus.limma@gmail.com>
2018-11-01 16:17:26 +0200

    Refactor coordinateConverter tests to use custom objects

--- Tests/UnitTests/CoordinateConverter/coordinateconvertertests.cc
+++ Tests/UnitTests/CoordinateConverter/coordinateconvertertests.cc
@@ -7,6 +7,9 @@
 using Common::CubeCoordinate;
 using Student::CartesianCoordinate;
 
+Q_DECLARE_METATYPE(CubeCoordinate);
+Q_DECLARE_METATYPE(CartesianCoordinate);
+
 class CoordinateConverterTests : public QObject {
   Q_OBJECT
 
@@ -21,33 +24,32 @@ private Q_SLOTS:
 CoordinateConverterTests::CoordinateConverterTests() {}
 
 void CoordinateConverterTests::testCoordinateConverting() {
-  QFETCH(int, cubeX);
-  QFETCH(int, cubeY);
-  QFETCH(int, cubeZ);
-  QFETCH(double, exceptedCartesianX);
-  QFETCH(double, exceptedCartesianY);
-
-  CubeCoordinate cubeCoord(cubeX, cubeY, cubeZ);
+  QFETCH(CubeCoordinate, cubeCoord);
+  QFETCH(CartesianCoordinate, excepted);
 
   CartesianCoordinate actual =
       Student::convertCubeCoordinatesToCartesian(cubeCoord);
 
-  QCOMPARE(actual.x, exceptedCartesianX);
-  QCOMPARE(actual.y, exceptedCartesianY);
+  QCOMPARE(actual.x, excepted.x);
+  QCOMPARE(actual.y, excepted.y);
 }
 
 void CoordinateConverterTests::testCoordinateConverting_data() {
-  QTest::addColumn<int>("cubeX");
-  QTest::addColumn<int>("cubeY");
-  QTest::addColumn<int>("cubeZ");
-  QTest::addColumn<double>("exceptedCartesianX");
-  QTest::addColumn<double>("exceptedCartesianY");
+  QTest::addColumn<CubeCoordinate>("cubeCoord");
+  QTest::addColumn<CartesianCoordinate>("excepted");
 
-  QTest::newRow("center") << 0 << 0 << 0 << 0.0 << 0.0;
-  QTest::newRow("along cube Z-axis") << +2 << -2 << 0 << 4 * sqrt(3) / 2 << 0.0;
-  QTest::newRow("random point 1") << +2 << -1 << -1 << 3 * sqrt(3) / 2 << 1.5;
-  QTest::newRow("random point 2") << -3 << 0 << +3 << -3 * sqrt(3) / 2 << -4.5;
-  QTest::newRow("random point 3") << -3 << +2 << +1 << -5 * sqrt(3) / 2 << -1.5;
+  QTest::newRow("center") << CubeCoordinate(0, 0, 0)
+                          << CartesianCoordinate(0.0, 0.0);
+  QTest::newRow("along cube Z-axis")
+      << CubeCoordinate(+2, -2, 0) << CartesianCoordinate(4 * sqrt(3) / 2, 0.0);
+  QTest::newRow("random point 1") << CubeCoordinate(+2, -1, -1)
+                                  << CartesianCoordinate(3 * sqrt(3) / 2, 1.5);
+  QTest::newRow("random point 2")
+      << CubeCoordinate(-3, 0, +3)
+      << CartesianCoordinate(-3 * sqrt(3) / 2, -4.5);
+  QTest::newRow("random point 3")
+      << CubeCoordinate(-3, +2, +1)
+      << CartesianCoordinate(-5 * sqrt(3) / 2, -1.5);
 }
 
 QTEST_APPLESS_MAIN(CoordinateConverterTests)
