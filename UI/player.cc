a088785fe2a50a680320c074fe6b0a7f61722ff7
Paulus Limma <paulus.limma@gmail.com>
2018-11-22 13:39:07 +0200

    Add Points-property to Player

--- UI/player.cc
+++ UI/player.cc
@@ -7,12 +7,10 @@ static const std::vector<Qt::GlobalColor> playerColors{
     Qt::GlobalColor::yellow};
 }
 
-
-
 namespace Student {
 
 Player::Player(int id, unsigned int actionsLeft, QString name)
-    : id_(id), actionsLeft_(actionsLeft), name_(name) {}
+    : id_(id), actionsLeft_(actionsLeft), name_(name), points_(0) {}
 
 int Player::getPlayerId() const { return id_; }
 
@@ -22,12 +20,14 @@ void Player::setActionsLeft(unsigned int actionsLeft) {
 
 unsigned int Player::getActionsLeft() const { return actionsLeft_; }
 
-QColor Player::getPlayerColor(int playerId)
-{
-    return QColor(playerColors[playerId]);
+QColor Player::getPlayerColor(int playerId) {
+  return QColor(playerColors[playerId]);
 }
 
+QString Player::getName() const { return name_; }
 
-QString Player::getName() { return name_; }
+void Player::setPoints(int points) { points_ = points; }
+
+int Player::getPoints() const { return points_; }
 
 } // namespace Student
